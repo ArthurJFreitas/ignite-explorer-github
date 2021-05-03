d4e040cc7f89f3dd37ba1725486651499e8b3405
Arthur Freitas <arthur.silva@qflash.com.br>
2021-05-03 19:05:19 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+const { runtime } = require("webpack");
+
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+        ['@babel/preset-react',
+            { runtime: 'automatic' }
+        ]
+    ]
+}
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/global.scss'
+
+const App = () => {
+    return (
+        <h1>Olá Reactjs</h1>
+    )
+}
+
+export { App }
